82e272049d9de3b4798805b7ef403e44d8d02c44
Steevenrb <steeven04toa@gmail.com>
2024-06-28 11:28:44 -0500

    Cambios

---
+++ app/Filament/Resources/UsuariosResource.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UsuariosResource\Pages;
+use App\Filament\Resources\UsuariosResource\RelationManagers;
+use App\Models\Usuario;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UsuariosResource extends Resource
+{
+    protected static ?string $model = Usuario::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('nombre')
+                ->required()
+                ->maxLength(255),
+                Forms\Components\TextInput::make('email')
+                ->required()
+                ->maxLength(255),
+                Forms\Components\TextInput::make('password')
+                ->required()
+                ->maxLength(255),
+                Forms\Components\Select::make('tipo')
+                ->options([
+                    'cliente' => 'Cliente',
+                    'admin' => 'Administrador',
+                ])
+                ->required(),
+
+            ]);
+    }
+
+    public static function table(Table $usuarios): Table
+    {
+        return $usuarios
+            ->columns([
+                Tables\Columns\TextColumn::make('nombre'),
+                Tables\Columns\TextColumn::make('email'),
+                Tables\Columns\TextColumn::make('password'),
+                Tables\Columns\TextColumn::make('tipo'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsuarios::route('/'),
+            'create' => Pages\CreateUsuarios::route('/create'),
+            'edit' => Pages\EditUsuarios::route('/{record}/edit'),
+        ];
+    }
+}
