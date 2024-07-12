629dd0e4d53dafb2a23319a8acebaa97b35fbd18
ElizabethHidalgo <aehidalgo3@espe.edu.ec>
2024-07-12 15:04:03 -0500

    Revert "Alex caisalitn"

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,6 @@ Route::get('/login', function () {
     return redirect('/admin/login');
 })->name('filament.auth.login');
 
-//<<<<<<< Alex-Caisalitn
-Livewire::setUpdateRoute(function($handle){
-    return Route::post('/Tienda_Estetica_Smart/public/livewire/update',$handle);
-});
-=======
 Route::get('/login', function () {
     return redirect(route('filament.admin.auth.login'));
 })->name('login');
-//>>>>>>> master
