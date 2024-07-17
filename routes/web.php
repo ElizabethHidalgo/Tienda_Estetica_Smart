f440ec79efaedebc8400cc16d35db39040ec2069
Steevenrb <steeven04toa@gmail.com>
2024-07-17 12:03:18 -0500

    commit
    solucion problema POST

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Filament\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductoController;
-
+use Livewire\Livewire;
 
 Route::get('/', function () {
     return view('app');
@@ -17,5 +17,8 @@ Route::get('/login', function () {
     return redirect(route('filament.admin.auth.login'));
 })->name('login');
 
+Livewire::setUpdateRoute(function($handle) {
+    return Route::post('/Tienda_Estetica_Smart/public/livewire/update', $handle);
+});
 
 Route::get('/productos', [ProductoController::class, 'index'])->name('productos.index');
