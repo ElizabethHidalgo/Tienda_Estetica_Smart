ce871bdbee268feb71b776a1fa24c93c4c33effb
Steevenrb <steeven04toa@gmail.com>
2024-08-05 12:30:18 -0500

    modificacion en la seccion productos

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -1,14 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\Categoria;
 use App\Models\Producto;
 use Illuminate\Http\Request;
 
 class ProductoController extends Controller
 {
-    //
-    public function index(){
-        $productos = Producto::all();
-        return view('productos',['productos' => $productos]);
+    public function index(Request $request)
+    {
+        // Obtener todas las categorías
+        $categorias = Categoria::all();
+
+        // Filtrar productos por categoría si se pasa un parámetro 'categoria'
+        $productos = Producto::when($request->has('categoria'), function ($query) use ($request) {
+            return $query->where('categoria_id', $request->input('categoria'));
+        })->paginate(6);
+
+        return view('productos', [
+            'productos' => $productos,
+            'categorias' => $categorias
+        ]);
     }
 }
