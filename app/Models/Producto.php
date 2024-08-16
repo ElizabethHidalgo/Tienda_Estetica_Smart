4c096502ae14178f8db3b89f38df5b8114bfdbc8
Edison25G <158979263+Edison25G@users.noreply.github.com>
2024-08-16 08:26:21 -0500

    Update Producto.php

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -4,14 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Producto extends Model
 {
     use HasFactory;
-    protected $guarded = [];
-    
-    public function categoria() : BelongsTo {
-        return $this->belongsTo(Categoria::class);
-    }
+
+    protected $table = 'productos';
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'categoria_id',
+        'precio'
+    ];
 }
